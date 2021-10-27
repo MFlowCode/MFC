a94ac55b308140423a17fbac86cbdca3a4d3ce53
Spencer Bryngelson <sbryngelson@gmail.com>
2021-10-26 23:53:53 -0400

    fix license

--- src/simulation_code/m_riemann_solvers.f90
+++ src/simulation_code/m_riemann_solvers.f90
@@ -1,29 +1,3 @@
-!!       __  _______________
-!!      /  |/  / ____/ ____/
-!!     / /|_/ / /_  / /
-!!    / /  / / __/ / /___
-!!   /_/  /_/_/    \____/
-!!
-!!  This file is part of MFC.
-!!
-!!  MFC is the legal property of its developers, whose names
-!!  are listed in the copyright file included with this source
-!!  distribution.
-!!
-!!  MFC is free software: you can redistribute it and/or modify
-!!  it under the terms of the GNU General Public License as published
-!!  by the Free Software Foundation, either version 3 of the license
-!!  or any later version.
-!!
-!!  MFC is distributed in the hope that it will be useful,
-!!  but WITHOUT ANY WARRANTY; without even the implied warranty of
-!!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-!!  GNU General Public License for more details.
-!!
-!!  You should have received a copy of the GNU General Public License
-!!  along with MFC (LICENSE).
-!!  If not, see <http://www.gnu.org/licenses/>.
-
 !>
 !! @file m_riemann_solvers.f90
 !! @brief Contains module m_riemann_solvers
