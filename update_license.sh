1095b266f06a691f8adf12ed3ab171a4d8f983a4
Spencer Bryngelson <sbryngelson@gmail.com>
2021-10-26 23:57:22 -0400

    fix license

--- update_license.sh
+++ update_license.sh
@@ -5,5 +5,5 @@ FILES="*.f90"
 for f in $FILES; do
   echo "Processing $f file..."
   # cat "$f"
-    tail -n +2 "$f" > "$f.tmp" && mv "$f.tmp" "$f"
+    tail -n +27 "$f" > "$f.tmp" && mv "$f.tmp" "$f"
 done
