ac5c6604b3de7c45dee7d8010c0892fbca42c250
sbryngelson <sbryngelson@gmail.com>
2021-10-28 08:55:55 -0400

    update make and run files

---
+++ example_cases/1D_exercise_WENO/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# module purge
+# module load lsf-tools/2.0 DefApps nvhpc/21.9 spectrum-mpi/10.3.1.2-20200121 cuda/11.2.0
+
+export PGI_ACC_NOTIFY=
+export PGI_ACC_DEBUG=
+jsrun -r1 -a1 -c1 -g1 ../../src/pre_process_code/pre_process
+jsrun -r1 -a1 -c1 -g1 ../../src/simulation_code/simulation
+
---
+++ load.sh
@@ -0,0 +1,5 @@
+# For Ascent
+module purge
+module load nvhpc/21.9   
+module load spectrum-mpi
+module load cuda/11.2.0
