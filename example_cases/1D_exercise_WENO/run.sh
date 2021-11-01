0c3ec1deb72d43389fba102f6abf316f4be6319d
sbryngelson <sbryngelson@gmail.com>
2021-11-01 10:19:58 -0400

    debug

--- example_cases/1D_exercise_WENO/run.sh
+++ example_cases/1D_exercise_WENO/run.sh
@@ -2,8 +2,10 @@
 # module purge
 # module load lsf-tools/2.0 DefApps nvhpc/21.9 spectrum-mpi/10.3.1.2-20200121 cuda/11.2.0
 
+
+
 export PGI_ACC_NOTIFY=
-export PGI_ACC_DEBUG=
+export PGI_ACC_DEBUG=1
 jsrun -r1 -a1 -c1 -g1 ../../src/pre_process_code/pre_process
 jsrun -r1 -a1 -c1 -g1 ../../src/simulation_code/simulation
 
