0077bda9f7aad2c6ff4f5c492748c7a57b5a7a2e
Anand Radhakrishnan <aradhakr34@login1.ascent.olcf.ornl.gov>
2021-11-03 21:04:34 -0400

    redundant reconstruct fix

--- example_cases/1D_exercise_WENO/run.sh
+++ example_cases/1D_exercise_WENO/run.sh
@@ -4,8 +4,9 @@
 
 
 
+export PGI_ACC_TIME=1
 export PGI_ACC_NOTIFY=
-export PGI_ACC_DEBUG=1
+export PGI_ACC_DEBUG=
 jsrun -r1 -a1 -c1 -g1 ../../src/pre_process_code/pre_process
-jsrun -r1 -a1 -c1 -g1 ../../src/simulation_code/simulation
+time jsrun -r1 -a1 -c1 -g1 ../../src/simulation_code/simulation
 
