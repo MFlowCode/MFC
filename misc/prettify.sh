8135f04fe53f4b79642c30ed4395d6e1c89efb12
Henry Le Berre <hleberre64@gmail.com>
2022-01-16 00:52:42 -0500

    Bootstrap folder, more renaming, and mfc.sh to ensure python3 is working.

---
+++ misc/prettify.sh
@@ -0,0 +1,6 @@
+cd ../src/post_process_code
+fprettify -i 4 --case 1 1 1 1 -l 1000 -r ../
+# fprettify --strict-indent --diff --enable-replacements -i 4 --case 1 1 1 1 -l 1000 -r ../
+
+# cd ../src/pre_process_code
+# fprettify --strict-indent --diff --enable-replacements -i 4 --case 1 1 1 1 -l 1000 -r ../
