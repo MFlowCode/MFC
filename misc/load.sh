0a9bd0c1b571aaa23665ee3ba876e54d41f95968
Henry Le Berre <henrylb@batch5.summit.olcf.ornl.gov>
2022-01-18 14:35:24 -0500

    Fix LD_LIBRARY_PATH

--- misc/load.sh
+++ misc/load.sh
@@ -1,9 +1,24 @@
-# For Ascent
-module purge
-module load nvhpc/21.9   
-module load spectrum-mpi
-module load cuda/11.2.0
 
-module load nsight-compute
-module load nsight-systems
+echo -n "Would you like to load modules for Summit (s) or Ascent (a)? (s/a): "
+read response
 
+if [ "$response" == "s" ]; then # For Summit
+    module load lsf-tools/2.0
+    module load darshan-runtime/3.3.1-lite
+    module load DefApps
+    module load spectrum-mpi/10.4.0.3-20210112
+    module load hsi/5.0.2.p5
+    module load xalt/1.2.1
+    module load nvhpc/21.9
+    module load cuda/11.4.2
+elif [ "$response" == "a" ]; then # For Ascent
+    module purge
+    module load nvhpc/21.9
+    module load spectrum-mpi
+    module load cuda/11.2.0
+    module load nsight-compute
+    module load nsight-systems
+else
+    echo "Error: Requested system is not supported"
+    exit 1
+fi
