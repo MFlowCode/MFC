2f9b6e63f6fc56b8a9ffc53c95e3d7db60e6f167
Henry LE BERRE <hleberre64@gmail.com>
2022-01-18 21:16:08 -0500

    Add Bridges2 modules to load.sh

--- misc/load.sh
+++ misc/load.sh
@@ -1,8 +1,15 @@
+#!/usr/bin/env bash
 
-echo -n "Would you like to load modules for Summit (s) or Ascent (a)? (s/a): "
+
+echo "Which computer would like to laod submodules for?"
+echo "- Summit   (s)"
+echo "- Bridges2 (b)"
+echo "- Ascent   (a)"
+echo -n "(s/b/a): "
 read response
 
 if [ "$response" == "s" ]; then # For Summit
+    module purge
     module load lsf-tools/2.0
     module load darshan-runtime/3.3.1-lite
     module load DefApps
@@ -11,6 +18,12 @@ if [ "$response" == "s" ]; then # For Summit
     module load xalt/1.2.1
     module load nvhpc/21.9
     module load cuda/11.4.2
+    module list
+elif [ "$response" == "b" ]; then # Bridges2
+    module purge
+    module load openmpi/4.0.5-nvhpc21.7
+    module load cuda/11.1.1
+    module list
 elif [ "$response" == "a" ]; then # For Ascent
     module purge
     module load nvhpc/21.9
@@ -18,6 +31,7 @@ elif [ "$response" == "a" ]; then # For Ascent
     module load cuda/11.2.0
     module load nsight-compute
     module load nsight-systems
+    module list
 else
     echo "Error: Requested system is not supported"
     exit 1
