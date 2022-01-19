7bd823f8a99584fb82c57457ff687f2ade1f7113
Anand Radhakrishnan <aradhakr34@batch1.summit.olcf.ornl.gov>
2022-01-19 14:19:09 -0500

    Fixing WENO for Macros

--- samples/3D_exercise/run.sh
+++ samples/3D_exercise/run.sh
@@ -9,11 +9,11 @@ export PGI_ACC_TIME=0
 export PGI_ACC_NOTIFY=
 export PGI_ACC_DEBUG=0
 
-for proc in 3
+for proc in 1
 do
-	size=$((100 * proc))
-	sed -i 's/m = .*/m = '"$size"'/' simulation.inp
-	sed -i 's/m = .*/m = '"$size"'/' pre_process.inp
+	#size=$((100 * proc))
+	#sed -i 's/m = .*/m = '"$size"'/' simulation.inp
+	#sed -i 's/m = .*/m = '"$size"'/' pre_process.inp
 	jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 ../../src/pre_process_code/pre_process
 	time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1  ../../src/simulation_code/simulation
 	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 nsys profile ../../src/simulation_code/simulation
