68b668127eeb59b47366ffcef322a422ecb240f4
Henry Le Berre <hleberre64@gmail.com>
2022-01-29 02:36:56 -0500

    Working --clean and --scratch

--- bootstrap/prepare_make.sh
+++ bootstrap/prepare_make.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-set -e
-set -x
-
 BUILD_DIR="./.build"
 
 mkdir -p "$BUILD_DIR"
 cp -u *.f90    "$BUILD_DIR"
 cp -u Makefile "$BUILD_DIR"
 
-for fpp_f in *.fpp; do
-	f90_f=$(echo "$fpp_f" | sed s/.fpp/.f90/g)
-
-	if [ -f "$BUILD_DIR/$f90_f" ]; then
-		rm "$BUILD_DIR/$f90_f"
+for fpp_f in "$(find . -name "*.fpp" -not -path "$BUILD_DIR/*")"; do
+	if [ -f "$fpp_f" ]; then
+		f90_f=$(echo "$fpp_f" | sed s/.fpp/.f90/g)
+		fypp "$fpp_f" "$BUILD_DIR/$f90_f"
+		if (($?)); then
+			echo "[bootstrap/prepare_make.sh] Failed to execute fypp with:"
+			echo " - Base:        '$(pwd)'"
+			echo " - Source:      '$fpp_f'"
+			echo " - Destination: '$BUILD_DIR/$f90_f'"
+			exit 1
+		fi
 	fi
-
-	fypp "$fpp_f" "$BUILD_DIR/$f90_f"
 done
 
 make -C "$BUILD_DIR" "$@"
+exit $?
