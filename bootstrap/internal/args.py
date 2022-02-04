054f37b022aa6b8444d43d2dc92a72584e363742
Henry Le Berre <hleberre64@gmail.com>
2022-02-04 18:02:14 -0500

    MFC user configuration file & Tree-like mfc.sh console output

--- bootstrap/internal/args.py
+++ bootstrap/internal/args.py
@@ -1,14 +1,14 @@
 import argparse
 
+import internal.user       as user
 import internal.conf       as conf
-import internal.common     as common
 import internal.objecttree as objecttree
 
 class MFCArgs(objecttree.ObjectTree):
-    def __init__(self, conf: conf.MFCConf):
+    def __init__(self, conf: conf.MFCConf, user: user.MFCUser):
         parser = argparse.ArgumentParser(description="Wecome to the MFC master script.", )
 
-        compiler_configuration_names = [e.name for e in conf.configurations]
+        compiler_configuration_names = [e.name for e in user.configurations]
 
         grp_func = parser.add_argument_group(title="Action")
         grp_func = grp_func.add_mutually_exclusive_group()
@@ -25,15 +25,17 @@ class MFCArgs(objecttree.ObjectTree):
                             help="The space-separated targets you wish to have built.")
 
         parser.add_argument("-cc", "--compiler-configuration", type=str,
-                            choices=compiler_configuration_names, default="release-cpu")
+                            choices=compiler_configuration_names, default=user.defaults.configuration)
 
         parser.add_argument("-j", "--jobs", metavar="N", type=int,
-                            help="Allows for N concurrent jobs.", default=1)
+                            help="Allows for N concurrent jobs.", default=int(user.defaults.threads))
 
         parser.add_argument("-s", "--scratch", action="store_true",
                             help="Build all targets from scratch.")
 
         super().__init__(vars(parser.parse_args()))
+
+        print(self)
 
         if not self.tree_get("build") and not self.tree_get("test") and not self.tree_get("clean"):
             self.tree_set("build", True)
