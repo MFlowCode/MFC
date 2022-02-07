cf361a3ee308f072d69aa1731f003d01c79fd37a
Henry Le Berre <hleberre64@gmail.com>
2022-02-06 23:21:54 -0500

    Rename '.mfc/' to 'build/'

--- bootstrap/internal/common.py
+++ bootstrap/internal/common.py
@@ -8,7 +8,7 @@ import tarfile
 
 
 MFC_ROOTDIR       = os.path.normpath(f"{os.path.dirname(os.path.realpath(__file__))}/../..")
-MFC_SUBDIR        = f"{MFC_ROOTDIR}/.mfc"
+MFC_SUBDIR        = f"{MFC_ROOTDIR}/build"
 MFC_DEV_FILEPATH  = f"{MFC_ROOTDIR}/bootstrap/mfc.dev.yaml"
 MFC_USER_FILEPATH = f"{MFC_ROOTDIR}/mfc.user.yaml"
 MFC_LOCK_FILEPATH = f"{MFC_SUBDIR}/mfc.lock.yaml"
