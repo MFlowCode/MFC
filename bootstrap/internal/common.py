0e5622703134e8a5da355e8d2cfb50c21533c4f2
Henry A. Le Berre <hleberre64@gmail.com>
2022-02-15 18:27:54 -0500

    Fix #86

--- bootstrap/internal/common.py
+++ bootstrap/internal/common.py
@@ -86,21 +86,6 @@ def create_directory_safe(directory_path: str) -> None:
     os.makedirs(directory_path, exist_ok=True)
 
 
-def center_ansi_escaped_text(message: str) -> str:
-    nCols = shutil.get_terminal_size((80, 20)).columns
-
-    to_escape = [re.escape(colorama.Style.RESET_ALL)]
-    for key in dir(colorama.Fore):
-        if not callable(getattr(colorama.Fore, key)) and not key.startswith("__"):
-            to_escape.append(re.escape(getattr(colorama.Fore, key)))
-
-    longest_string_len = max([len(re.compile("|".join(to_escape), flags=re.DOTALL).sub("", line)) for line in message.splitlines()])
-
-    padding = " "*((nCols - longest_string_len) // 2)
-
-    return "\n".join([f'{padding}{line}{padding}' for line in message.splitlines()])
-
-
 def clear_print(message, end='\n') -> None:
     clear_line()
     print(message, end=end)
