3b9f331e63fff6a1a86e3d8fd0ba4ecde97867cb
Henry A. Le Berre <hleberre64@gmail.com>
2022-02-24 04:47:45 -0500

    Experimental version of new testing system working

--- bootstrap/internal/common.py
+++ bootstrap/internal/common.py
@@ -7,13 +7,13 @@ import colorama # *: Colorama package
 import tarfile
 
 
-MFC_ROOTDIR       = os.path.normpath(f"{os.path.dirname(os.path.realpath(__file__))}/../..")
-MFC_TESTDIR       = f"{MFC_ROOTDIR}/tests"
-MFC_SUBDIR        = f"{MFC_ROOTDIR}/build"
-MFC_DEV_FILEPATH  = f"{MFC_ROOTDIR}/bootstrap/mfc.dev.yaml"
-MFC_TEST_FILEPATH = f"{MFC_ROOTDIR}/bootstrap/mfc.test.yaml"
-MFC_USER_FILEPATH = f"{MFC_ROOTDIR}/mfc.user.yaml"
-MFC_LOCK_FILEPATH = f"{MFC_SUBDIR}/mfc.lock.yaml"
+MFC_ROOTDIR         = os.path.normpath(f"{os.path.dirname(os.path.realpath(__file__))}/../..")
+MFC_TESTDIR         = f"{MFC_ROOTDIR}/tests"
+MFC_SUBDIR          = f"{MFC_ROOTDIR}/build"
+MFC_DEV_FILEPATH    = f"{MFC_ROOTDIR}/bootstrap/mfc.dev.yaml"
+MFC_GOLDEN_FILEPATH = f"{MFC_ROOTDIR}/tests/golden.txt"
+MFC_USER_FILEPATH   = f"{MFC_ROOTDIR}/mfc.user.yaml"
+MFC_LOCK_FILEPATH   = f"{MFC_SUBDIR}/mfc.lock.yaml"
 
 
 class MFCException(Exception):
