fa07334399459d09451907afb38d9f86693d47a3
Henry A. Le Berre <hleberre64@gmail.com>
2022-02-24 11:39:03 -0500

    Add -g & better test case directory names

--- src/master_scripts/m_python_proxy.py
+++ src/master_scripts/m_python_proxy.py
@@ -1774,11 +1774,14 @@ def f_execute_mfc_component(comp_name: str, case_dict, mfc_dir, engine): # -----
         print( '\n' + comp_name + '>> Serial job in progress ...' + '\n')
         #cmd_status = Popen('./'+comp_dir+'/'+comp_name, shell=True, stdout=PIPE, universal_newlines=True)
 
+        print("RUNNING: ", os.getcwd())
+
         cmd_status = Popen(f'LD_LIBRARY_PATH="$LD_LIBRARY_PATH:{pathlib.Path(__file__).parent.resolve()}/../../build/common/build/lib" mpirun -n {str(pbs_dict["ppn"])} "{mfc_dir}/../build/___current___/build/bin/{comp_name}"', shell=True, universal_newlines=True)
         output, errors = cmd_status.communicate()
         if (cmd_status.returncode != 0):
             exit(cmd_status.returncode)
         #print '\n' + output
+        print("DONE: ", os.getcwd(), cmd_status.returncode)
         print( comp_name + '>> Serial job completed!' + '\n')
         #cmd_status = Popen('rm -f '+ comp_name +'.inp', shell=True, stdout=PIPE, universal_newlines=True)
         #output, errors = cmd_status.communicate()
