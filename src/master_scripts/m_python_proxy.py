814d786dcf575e626d1dc437998b8597bdb1d35c
Henry A. Le Berre <24208000+henryleberre@users.noreply.github.com>
2022-03-30 18:51:10 -0400

    CI: Ascent GPU CI & jsrun for testing

--- src/master_scripts/m_python_proxy.py
+++ src/master_scripts/m_python_proxy.py
@@ -1774,7 +1774,7 @@ def f_execute_mfc_component(comp_name: str, case_dict, mfc_dir, engine): # -----
         print( '\n' + comp_name + '>> Serial job in progress ...' + '\n')
         #cmd_status = Popen('./'+comp_dir+'/'+comp_name, shell=True, stdout=PIPE, universal_newlines=True)
 
-        cmd_status = Popen(f'LD_LIBRARY_PATH="$LD_LIBRARY_PATH:{pathlib.Path(__file__).parent.resolve()}/../../build/common/build/lib" mpirun -n {str(pbs_dict["ppn"])} "{mfc_dir}/../build/___current___/build/bin/{comp_name}"', shell=True, universal_newlines=True)
+        cmd_status = Popen(f'LD_LIBRARY_PATH="$LD_LIBRARY_PATH:{pathlib.Path(__file__).parent.resolve()}/../../build/common/build/lib" jsrun -r{str(pbs_dict["ppn"])} -g1 -c1 -a1 "{mfc_dir}/../build/___current___/build/bin/{comp_name}"', shell=True, universal_newlines=True)
         output, errors = cmd_status.communicate()
         if (cmd_status.returncode != 0):
             exit(cmd_status.returncode)
