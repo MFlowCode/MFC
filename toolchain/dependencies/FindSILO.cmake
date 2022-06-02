e21ed807726795df79a9e205844180b8b672d138
Henry A. Le Berre <24208000+henryleberre@users.noreply.github.com>
2022-06-01 20:12:29 -0400

    Use find_package & create FindSILO

---
+++ toolchain/dependencies/FindSILO.cmake
@@ -0,0 +1,31 @@
+# Attempt to find LLNL's Silo
+# URL:  https://github.com/LLNL/Silo 
+# DOCS: https://cmake.org/cmake/help/latest/command/find_library.html
+#       https://cmake.org/cmake/help/latest/module/FindPackageHandleStandardArgs.html
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+
+FIND_PATH(SILO_INSTALL_PREFIX NAMES include/silo.h)
+
+FIND_LIBRARY(
+    SILO_LIBRARIES
+    NAMES "siloh5"
+          "silo"
+    HINTS "${SILO_INSTALL_PREFIX}/lib"
+          "${SILO_INSTALL_PREFIX}/lib64"
+)
+
+FIND_PATH(
+    SILO_INCLUDE_DIRS
+    NAMES silo.h
+    HINTS ${SILO_INSTALL_PREFIX}/include
+)
+
+# CMake Magic
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+    SILO DEFAULT_MSG
+    SILO_LIBRARIES
+    SILO_INCLUDE_DIRS
+)
+
