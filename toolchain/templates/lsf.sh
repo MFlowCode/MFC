27706e5c3453a64e6aec8209c7cbda6b0a10338f
Henry Le Berre <hberre3@gatech.edu>
2022-07-03 17:41:03 -0400

    Fix Batch Execution

--- toolchain/templates/lsf.sh
+++ toolchain/templates/lsf.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #>
-#>           - LSF Batch File Template -             
+#>           - LSF Batch File Template -
 #>
 #> This file is part of the ./mfc.sh run subsystem.
 #> For more information, please consult the README.
-#> 
+#>
 #> - You are invited to modify this file to suit your
 #>   needs, in order to get MFC running properly on
 #>   your system.
@@ -23,7 +23,7 @@
 #>
 #> - Statements of the form {MFC::expression} tell MFC
 #>   where to place the common code, across all batch
-#>   files that is required to run MFC. They are not 
+#>   files that is required to run MFC. They are not
 #>   intended to be modified by users.
 #>
 #BSUB -J {name}
@@ -45,11 +45,11 @@
 
 
 
-#> 
+#>
 #> Note: If your system requires you to load environment
 #>       modules inside of your batch script, please load
 #>       them bellow.
-#> 
+#>
 
 
 
@@ -64,11 +64,11 @@
 #>       on your system - if at all. {MFC::BIN} refers to
 #>       the path the MFC executable.
 #>
-jsrun --smpiargs="-gpu"                      \
-      --nrs          {cpus_per_node*nodes}   \
-      --cpu_per_rs   1                       \
-      --gpu_per_rs   {min(gpus_per_node, 1)} \
-      --tasks_per_rs 1                       \
+jsrun {'--smpiargs="-gpu"' if gpus_per_node > 0 else ''} \
+      --nrs          {cpus_per_node*nodes}               \
+      --cpu_per_rs   1                                   \
+      --gpu_per_rs   {min(gpus_per_node, 1)}             \
+      --tasks_per_rs 1                                   \
       "{MFC::BIN}"
 
 {MFC::EPILOGUE}
