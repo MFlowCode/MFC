0557084b0751e4267c976f79fb9579d3b07a6d08
Henry Le Berre <hberre3@gatech.edu>
2022-09-30 20:02:27 -0700

    Fix #17 #20 #23 #24

--- examples/1D_bubblescreen/run.sh
+++ examples/1D_bubblescreen/run.sh
@@ -15,9 +15,9 @@ for proc in 1
 do
 	jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1  ../../build/___current___/build/bin/pre_process 
 	time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 ../../build/___current___/build/bin/simulation
-	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 ../src/simulation_code/simulation
-	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 nsys profile --stats=true ../../src/simulation_code/simulation
-	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 nv-nsight-cu-cli --set full -k m_weno_s_weno_alt_1341_gpu -f -o profile_anand_weno ../../src/simulation_code/simulation
-	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 nv-nsight-cu-cli --set full -k m_weno_s_weno_alt_1565_gpu -f -o profile_anand_mp_weno ../../src/simulation_code/simulation
+	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 ../src/simulation/simulation
+	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 nsys profile --stats=true ../../src/simulation/simulation
+	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 nv-nsight-cu-cli --set full -k m_weno_s_weno_alt_1341_gpu -f -o profile_anand_weno ../../src/simulation/simulation
+	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 nv-nsight-cu-cli --set full -k m_weno_s_weno_alt_1565_gpu -f -o profile_anand_mp_weno ../../src/simulation/simulation
 	#time jsrun --smpiargs="-gpu" -r$proc -a1 -c1 -g1 nv-nsight-cu-cli --set full -k m_riemann_solvers_s_hllc_riemann_solver_acc_2539_gpu -f -o profile_anand_riemann ../../.mfc/___current___/build/bin/simulation
 done
