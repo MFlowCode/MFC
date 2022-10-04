0901b6e403107e342536a091054d296f965a23bd
Anand <anand@lawn-128-61-50-61.lawn.gatech.edu>
2022-10-04 18:08:10 -0400

    Fixing examples bug

--- examples/2D_mixing_nobubble/save_case.py
+++ examples/2D_mixing_nobubble/save_case.py
@@ -65,7 +65,7 @@ print(json.dumps({
     'p'                            : 0,
     'dt'                           : dt,
     't_step_start'                 : t_start,
-    't_step_stop'                  : Nt,
+    't_step_stop'                  : int(Nt),
     't_step_save'                  : t_save ,
     # ==========================================================
 
