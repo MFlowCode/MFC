b564591d877656f65dacf2ebf878692a6bf88450
Ben Wilfong <48168887+wilfonba@users.noreply.github.com>
2022-12-07 13:44:53 -0500

    create m_helper.f90

---
+++ src/common/m_helper.f90
@@ -0,0 +1,26 @@
+!>
+!! @file m_derived_types.f90
+!! @brief Contains module m_derived_types
+
+!> @brief This file contains the definitions of all of the custom-defined
+!!              types used in the pre-process code.
+module m_helper
+
+    ! Dependencies =============================================================
+    
+    use m_derived_types        !< Definitions of the derived types
+
+    use m_global_parameters    !< Definitions of the global parameters
+
+    ! ==========================================================================
+
+    implicit none
+
+    private; public ::
+
+contains
+
+    
+
+
+end module
