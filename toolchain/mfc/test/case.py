99f155f6f58be29bb98f6c5a1eb5beeb06096b97
JRChreim <jrchreim@outlook.com>
2023-06-16 21:53:08 -0700

    3rd set of changes

--- toolchain/mfc/test/case.py
+++ toolchain/mfc/test/case.py
@@ -156,6 +156,11 @@ class TestCase(case.Case):
     def create_directory(self):
         dirpath = self.get_dirpath()
 
+        if delete:
+            common.delete_directory(os.path.join(dirpath, "D"))
+            common.delete_directory(os.path.join(dirpath, "p_all"))
+            common.delete_directory(os.path.join(dirpath, "silo_hdf5"))
+
         common.create_directory(dirpath)
 
         common.file_write(f"{dirpath}/case.py", f"""\
