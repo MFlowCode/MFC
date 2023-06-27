12e340d35efdbb99b092909ba92c1e22d1f8e567
Spencer Bryngelson <sbryngelson@gmail.com>
2023-06-27 10:27:08 -0400

    Update run-phoenix-release-gpu.sh

--- misc/run-phoenix-release-gpu.sh
+++ misc/run-phoenix-release-gpu.sh
@@ -2,7 +2,9 @@
 #SBATCH -Jshb-test-jobs                         # Job name
 #SBATCH --account=gts-sbryngelson3               # charge account
 #SBATCH -N1                                     # Number of nodes and cores per node required
-#SBATCH --gres=gpu:V100:2
+#SBATCH -CV100-16GB
+#SBATCH -G2
+#SBATCH --tasks-per-node=2
 #SBATCH -t 02:00:00                              # Duration of the job (Ex: 15 mins)
 #SBATCH -q embers                               # QOS Name
 #SBATCH -otest.out                               # Combined output and error messages file
