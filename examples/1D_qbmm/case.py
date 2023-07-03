5ecfef3238290236a3cbd4c0e462e76abeb55fb7
Anand <anand@Anands-MacBook-Pro.local>
2023-07-03 11:43:08 -0400

    MERGE

--- examples/1D_qbmm/case.py
+++ examples/1D_qbmm/case.py
@@ -5,17 +5,16 @@ import json
 
 x0      = 10.E-06
 p0      = 101325.
-rho0    = 1000.
-
+rho0    = 1.E+03
 c0      = math.sqrt( p0/rho0 )
 patm    = 1.
 
 #water props
 n_tait  = 7.1
-B_tait  = 306.E+06 / p0 
+B_tait  = 306.E+06 / p0
 mul0    = 1.002E-03     #viscosity
 ss      = 0.07275       #surface tension
-pv      = 2.3388E+03 
+pv      = 2.3388E+03    #vapor pressure
 
 gamma_v = 1.33
 M_v     = 18.02
@@ -35,15 +34,18 @@ gamma_gas = 1.4
 #reference bubble size
 R0ref   = 10.E-06
 
+pa      = 0.1 * 1.E+06 / 101325.
 
 #Characteristic velocity
 uu = math.sqrt( p0/rho0 )
 #Cavitation number
-Ca = (p0 - pv)/(rho0*(uu**2.))
+# Ca = (p0 - pv)/(rho0*(uu**2.))
+Ca = 1.
 #Weber number
-We =  p0*R0ref/ss
+# We = rho0*(uu**2.)*R0ref/ss
+We = p0*R0ref/ss
 #Inv. bubble Reynolds number
-Re_inv = mul0/(p0*R0ref)
+Re_inv = mul0/(rho0*uu*R0ref)
 
 #IC setup
 vf0     = 4.E-5
@@ -56,21 +58,22 @@ nbubbles = 1
 myr0    = R0ref
 
 cfl     = 0.1
-Nx      = 100
+Nx      = 400
 Ldomain = 20.E-03
 L       = Ldomain/x0
 dx      = L/float(Nx)
 dt      = cfl*dx*c0/cact
 Lpulse  = 0.3*Ldomain
 Tpulse  = Lpulse/cact
-Tfinal  = 0.4*10.*Tpulse*c0/x0
+Tfinal  = 0.25*10.*Tpulse*c0/x0
 Nt      = int(Tfinal/dt)
 
+dt = dt * 0.1
+# print('dt: ',dt)
 
 Nfiles = 20.
 Nout = int(math.ceil(Nt/Nfiles))
 Nt = int(Nout*Nfiles)
-
 
 # Configuring case dictionary
 print(json.dumps({
@@ -86,10 +89,12 @@ print(json.dumps({
     'm'                            : Nx,
     'n'                            : 0,
     'p'                            : 0,
-    'dt'                           : 1e-4,
+    'dt'                           : 0.002,
     't_step_start'                 : 0,
-    't_step_stop'                  : 10000,
-    't_step_save'                  : 10,
+    't_step_stop'                  : 8000,
+    # 't_step_stop'                  : 4,
+    't_step_save'                  : 8000,
+    # 't_step_save'                  : 1,
     # ==========================================================
 
     # Simulation Algorithm Parameters ==========================
@@ -101,7 +106,6 @@ print(json.dumps({
     'mpp_lim'                      : 'F',
     'mixture_err'                  : 'F',
     'time_stepper'                 : 3,
-    'weno_vars'                    : 2,
     'weno_order'                   : 5,
     'weno_eps'                     : 1.E-16,
     'mapped_weno'                  : 'T',
@@ -110,8 +114,8 @@ print(json.dumps({
     'riemann_solver'               : 2,
     'wave_speeds'                  : 1,
     'avg_state'                    : 2,
-    'bc_x%beg'                     : -3,
-    'bc_x%end'                     : -3,
+    'bc_x%beg'                     : -8,
+    'bc_x%end'                     : -8,
     # ==========================================================
 
     # Formatted Database Files Structure Parameters ============
@@ -121,7 +125,7 @@ print(json.dumps({
     'parallel_io'                  :'F',
     'fd_order'                     : 1,
     #'schlieren_wrt'                :'T',
-    'probe_wrt'                    :'F',
+    'probe_wrt'                    :'T',
     'num_probes'                   : 1,
     'probe(1)%x'                   : 0.,
     # ==========================================================
@@ -131,8 +135,8 @@ print(json.dumps({
     'patch_icpp(1)%x_centroid'     : 0.,
     'patch_icpp(1)%length_x'       : 20.E-03/x0,
     'patch_icpp(1)%vel(1)'         : 0.0,
-    'patch_icpp(1)%pres'           : 101325/p0,
-    'patch_icpp(1)%alpha_rho(1)'   : (1.-1.E-12)*1000/rho0,
+    'patch_icpp(1)%pres'           : 1,
+    'patch_icpp(1)%alpha_rho(1)'   : (1.-1.E-12)*1.E+03/rho0,
     'patch_icpp(1)%alpha(1)'       : 1.E-12,
     'patch_icpp(1)%r0'             : 1.,
     'patch_icpp(1)%v0'             : 0.0E+00,
@@ -141,18 +145,18 @@ print(json.dumps({
     # Patch 2 Screen ===========================================
     'patch_icpp(2)%geometry'       : 1,
     'patch_icpp(2)%x_centroid'     : 0.,
-    'patch_icpp(2)%length_x'       : 10.E-03/x0,
+    'patch_icpp(2)%length_x'       : 5.E-03/x0,
     # 'patch_icpp(1)%length_x'       : 20.E-03/x0,
     'patch_icpp(2)%alter_patch(1)' : 'T',
     'patch_icpp(2)%vel(1)'         : 0.0,
-    'patch_icpp(2)%pres'           : 10*101325/p0,
-    'patch_icpp(2)%alpha_rho(1)'   : (1.-vf0)*1000/rho0,
+    'patch_icpp(2)%pres'           : 1,
+    'patch_icpp(2)%alpha_rho(1)'   : (1.-vf0)*1.E+03/rho0,
     'patch_icpp(2)%alpha(1)'       : vf0,
     'patch_icpp(2)%r0'             : 1.,
     'patch_icpp(2)%v0'             : 0.,
     # ==========================================================
 
-    # Fluids Physical Parameters ===============================================
+    # Fluids Physical Parameters ===============================
     # Surrounding liquid
     'fluid_pp(1)%gamma'             : 1.E+00/(n_tait-1.E+00),
     'fluid_pp(1)%pi_inf'            : n_tait*B_tait/(n_tait-1.),
@@ -164,16 +168,17 @@ print(json.dumps({
     'fluid_pp(1)%mu_v'              : mu_v,
     'fluid_pp(1)%k_v'               : k_v,
 
-    # Last fluid_pp is always reserved for bubble gas state ===================
-    # if applicable  ==========================================================
+    # Last fluid_pp is always reserved for bubble gas state ===
+    # if applicable  ==========================================
     'fluid_pp(2)%gamma'             : 1./(gamma_gas-1.),
     'fluid_pp(2)%pi_inf'            : 0.0E+00,
     'fluid_pp(2)%gamma_v'           : gamma_n,
     'fluid_pp(2)%M_v'               : M_n,
     'fluid_pp(2)%mu_v'              : mu_n,
     'fluid_pp(2)%k_v'               : k_n,
-    
-    #Non-polytropic gas compression model AND/OR Tait EOS =====
+    # ==========================================================
+
+    # Non-polytropic gas compression model AND/OR Tait EOS =====
     'pref'                  : p0,
     'rhoref'                : rho0,
     # ==========================================================
@@ -181,23 +186,34 @@ print(json.dumps({
     # Bubbles ==================================================
     'bubbles'               : 'T',
     'bubble_model'          : 2,
-    'polytropic'            : 'F',
-    'polydisperse'          : 'F',
+    'polytropic'            : 'T',
+    'polydisperse'          : 'T',
     'R0_type'               : 1,
     'poly_sigma'            : 0.3,
     'thermal'               : 3,
-    'Ca'                    : Ca,	
     'R0ref'                 : myr0,
+    # 'nb'                    : 3,
     'nb'                    : 3,
-    'Web'                   : We,
-    'Re_inv'                : Re_inv,
+    'Ca'                    : Ca,
+    # 'Web'                   : We,
+    # 'Re_inv'                : Re_inv,
     'qbmm'               : 'T',
-    'dist_type'          : 1,
+    'dist_type'          : 2,
     'sigR'               : 0.1,
     'sigV'               : 0.1,
     'rhoRV'              : 0.0,
     # ==========================================================
 
+    # Acoustic source ==========================================
+    'Monopole'                  : 'T',
+    'num_mono'                  : 1,
+    'Mono(1)%loc(1)'            : -5.E-03/x0,
+    'Mono(1)%npulse'            : 1,
+    'Mono(1)%dir'               : 1.,
+    'Mono(1)%pulse'             : 1,
+    'Mono(1)%mag'               : 1*pa,
+    'Mono(1)%length'            : (1./(300000.))*cact/x0,
+    # ==========================================================
 }))
 
 # ==============================================================================
