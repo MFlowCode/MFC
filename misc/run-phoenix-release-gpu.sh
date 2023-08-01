c7e26f2115aab75f32f7552c76f9c2efbc6a79c4
Henry LE BERRE <hberre3@gatech.edu>
2023-07-31 22:05:24 -0700

    Support ./mfc.sh test on multiple GPUs

--- misc/run-phoenix-release-gpu.sh
+++ misc/run-phoenix-release-gpu.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#SBATCH -Jshb-test-jobs                         # Job name
-#SBATCH --account=gts-sbryngelson3               # charge account
-#SBATCH -N1                                     # Number of nodes and cores per node required
+#SBATCH -Jshb-test-jobs            # Job name
+#SBATCH --account=gts-sbryngelson3 # charge account
+#SBATCH -N1                        # Number of nodes and cores per node required
 #SBATCH -CV100-16GB
 #SBATCH -G2
-#SBATCH -t 02:00:00                              # Duration of the job (Ex: 15 mins)
-#SBATCH -q embers                               # QOS Name
-#SBATCH -otest.out                               # Combined output and error messages file
-#SBATCH -W                                      # Do not exit until the submitted job terminates.
+#SBATCH -t 02:00:00                # Duration of the job (Ex: 15 mins)
+#SBATCH -q embers                  # QOS Name
+#SBATCH -otest.out                 # Combined output and error messages file
+#SBATCH -W                         # Do not exit until the submitted job terminates.
 
-cd $SLURM_SUBMIT_DIR                            # Change to working directory
-echo $(pwd)
+cd "$SLURM_SUBMIT_DIR"
+echo "Running in $(pwd):"
+
+set -x
 
 . ./mfc.sh load -c p -m GPU
 
-nvidia-smi
-echo $(nproc)
+./mfc.sh test -a -b mpirun -j $(nproc) \
+              --gpu -g $(seq -s ',' 0 $(($(nvidia-smi -L | wc -l)-1)))
 
-
-./mfc.sh test -b mpirun -a --gpu
