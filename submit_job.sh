b48993184016a0d08ba454fe0f39222ec69869e5
Mauro Rodriguez <mauro_rodriguez@brown.edu>
2023-10-26 17:48:28 -0400

    submit file change

--- submit_job.sh
+++ submit_job.sh
@@ -10,9 +10,7 @@
 #./mfc.sh run /anvil/projects/x-mch220010/rc/input.py -e batch -p wholenode -w 04:00:00 -# rc -N 2 -n 128 -t post_process -b mpirun
 
 #./mfc.sh run /anvil/projects/x-mch220010/dfd2023/base/case_mfc.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t pre_process -b mpirun
-#./mfc.sh run /anvil/projects/x-mch220010/dfd2023/base/input.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t simulation -b mpirun
+./mfc.sh run /anvil/projects/x-mch220010/dfd2023/base/input.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t post_process -b mpirun
 
-#./mfc.sh run ~/../../anvil/projects/x-mch220010/dfd2023/base_test/input.py -e batch -p wholenode -w 01:00:00 -# base -N 2 -n 128 -t pre_process -b mpirun
 
-./mfc.sh run ~/../../anvil/projects/x-mch220010/test/input_sawyer.py -e batch -p wholenode -w 01:00:00 -# base -N 2 -n 128 -t pre_process -b mpirun
 
