ea76937d7c70c686ad8d5860cb6c4026996a65b2
Abbott, Stephen R <abbottsr@ornl.gov>
2023-11-03 10:57:49 -0400

    Add magic speedup script for the Cray Compiler
    On Frontier, after building simulation run the script
    ./cce_simulation_workgroup_256.sh to edit the workgroup
    size in the generated code and recompile, then set the recommended
    environment variable to use the new code.
    
    Using the script requires `-hkeepfiles` at link!
    
    This script needs to be run after every time simulation
    is built to generate a correct offload binary.
    This include case-optimization builds!

---
+++ cce_simulation_workgroup_256.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+CCE_LLVM_PATH=${CRAY_CCE_CLANGSHARE}/../
+
+WGSIZE=256
+
+echo "Disassembling"
+${CCE_LLVM_PATH}/bin/llvm-dis build/simulation/simulation-cce-openmp-pre-llc.bc
+
+echo "Globally setting amdgpu-flat-work-group-size size to 1,$WGSIZE"
+sed "s/\"amdgpu-flat-work-group-size\"\=\"1,1024\"/\"amdgpu-flat-work-group-size\"\=\"1,${WGSIZE}\"/g" build/simulation/simulation-cce-openmp-pre-llc.bc > build/simulation/simulation-cce-openmp-pre-llc-wg${WGSIZE}.ll
+
+echo "Invoking LLC to compile"
+${CCE_LLVM_PATH}/bin/llc -mtriple=amdgcn-amd-amdhsa -disable-promote-alloca-to-lds -mcpu=gfx90a -amdgpu-dump-hsa-metadata build/simulation/simulation-cce-openmp-pre-llc-wg${WGSIZE}.ll -filetype=obj -o build/simulation/simulation-cce-openmp__llc_wg${WGSIZE}.amdgpu
+
+echo "Linking to a CCE Offload module"
+${CCE_LLVM_PATH}/bin/lld  -flavor gnu --no-undefined -shared -o build/simulation/simulation-wg${WGSIZE}.lld.exe build/simulation/simulation-cce-openmp__llc_wg${WGSIZE}.amdgpu
+
+echo "Now "
+echo "export CRAY_ACC_MODULE=${PWD}/build/simulation/simulation-wg${WGSIZE}.lld.exe"
+echo "to use the new GPU offload code."
+echo "To use the original build"
+echo "unset CRAY_ACC_MODULE"
