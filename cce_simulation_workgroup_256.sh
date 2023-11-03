77b9dacd1864059203516b9a949ea1c018c1527b
Abbott, Stephen R <abbottsr@ornl.gov>
2023-11-03 11:10:56 -0400

    Fix a bug in the workgroup touchup script

--- cce_simulation_workgroup_256.sh
+++ cce_simulation_workgroup_256.sh
@@ -8,7 +8,7 @@ echo "Disassembling"
 ${CCE_LLVM_PATH}/bin/llvm-dis build/simulation/simulation-cce-openmp-pre-llc.bc
 
 echo "Globally setting amdgpu-flat-work-group-size size to 1,$WGSIZE"
-sed "s/\"amdgpu-flat-work-group-size\"\=\"1,1024\"/\"amdgpu-flat-work-group-size\"\=\"1,${WGSIZE}\"/g" build/simulation/simulation-cce-openmp-pre-llc.bc > build/simulation/simulation-cce-openmp-pre-llc-wg${WGSIZE}.ll
+sed "s/\"amdgpu-flat-work-group-size\"\=\"1,1024\"/\"amdgpu-flat-work-group-size\"\=\"1,${WGSIZE}\"/g" build/simulation/simulation-cce-openmp-pre-llc.ll > build/simulation/simulation-cce-openmp-pre-llc-wg${WGSIZE}.ll
 
 echo "Invoking LLC to compile"
 ${CCE_LLVM_PATH}/bin/llc -mtriple=amdgcn-amd-amdhsa -disable-promote-alloca-to-lds -mcpu=gfx90a -amdgpu-dump-hsa-metadata build/simulation/simulation-cce-openmp-pre-llc-wg${WGSIZE}.ll -filetype=obj -o build/simulation/simulation-cce-openmp__llc_wg${WGSIZE}.amdgpu
