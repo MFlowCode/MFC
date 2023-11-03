ef89a312c9f6620cc6459d6aaa3dfec3bc5917ce
Mauro Rodriguez <mauro_rodriguez@brown.edu>
2023-11-03 01:18:05 -0400

    updated submit job script

--- submit_job.sh
+++ submit_job.sh
@@ -3,7 +3,8 @@
 #./mfc.sh run /anvil/projects/x-mch220010/undex/input.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t pre_process -b mpirun
 #./mfc.sh run /anvil/projects/x-mch220010/rc/input.py -e batch -p wholenode -w 01:00:00 -# rc -N 2 -n 128 -t pre_process -b mpirun
 
-#./mfc.sh run /anvil/projects/x-mch220010/mrdz/mfcruns/test/input.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t pre_process -b mpirun
-./mfc.sh run /anvil/projects/x-mch220010/mrdz/mfcruns/test/input.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t simulation -b mpirun
 
+./mfc.sh run /anvil/projects/x-mch220010/mrdz/mfcruns/test/input.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t pre_process -b mpirun
+#./mfc.sh run /anvil/projects/x-mch220010/mrdz/mfcruns/test/input.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t simulation -b mpirun
+#./mfc.sh run /anvil/projects/x-mch220010/mrdz/mfcruns/test/input.py -e batch -p wholenode -w 01:00:00 -# undex -N 2 -n 128 -t post_process -b mpirun
 
