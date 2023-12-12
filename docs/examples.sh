89d48c20bc24e41f7266d003531082659675f23c
Henry LE BERRE <hberre3@gatech.edu>
2023-12-11 22:20:48 -0800

    #245: Fix duplicated examples.md items in website's docs

--- docs/examples.sh
+++ docs/examples.sh
@@ -7,19 +7,15 @@ rm "$examples_md" || true
 
 echo -e "# Example Cases\n" > "$examples_md"
 
-for casedir in $(find "$1/examples/" -maxdepth 1 -type d); do
-    casename="$(basename $casedir)"
-
-    if [ "$casename" == "examples" ]; then
-        continue
-    fi
+for casedir in $(find "$1/examples/" -mindepth 1 -maxdepth 1 -type d); do
+    casename="$(basename "$casedir")"
 
     if [ -f "$casedir/README.md" ]; then
-        sed -e "s/\.png/-$casename\.png/g" "$casedir/README.md" | sed 's/^#/##/g' >> "$examples_md"
-        echo "" >> "$examples_md"
+        sed -e "s/\.png/-$casename-example\.png/g" "$casedir/README.md" | sed 's/^#/##/g' >> "$examples_md"
+        echo '' >> "$examples_md"
 
-        for png in $(find "$casedir" -maxdepth 1 -name "*.png"); do
-            cp "$png" "$1/docs/documentation/$(basename $png | sed s/\.png//g)-$casename.png"
+        for png in $(find "$casedir" -maxdepth 1 -name '*.png'); do
+            cp "$png" "$1/docs/documentation/$(basename "$png" | sed s/\.png//g)-$casename-example.png"
         done
     fi
 done
