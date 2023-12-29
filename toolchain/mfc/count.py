47aad98c9885cda91d3dc30e8ee02fb8b114f87d
Spencer Bryngelson <sbryngelson@gmail.com>
2023-12-29 04:13:00 +0000

    Update count.py

--- toolchain/mfc/count.py
+++ toolchain/mfc/count.py
@@ -12,7 +12,13 @@ def handle_dir(dirpath: str) -> typing.Tuple[typing.List[typing.Tuple[str, int]]
 
     for filepath in glob.glob(os.path.join(dirpath, '*.*f*')):
         with open(filepath) as f:
-            count = sum(1 if not l.isspace() else 0 for l in f.read().split('\n'))
+            count = 0
+            for l in f.read().split('\n'):
+                if not (l.isspace() or len(l) == 0):
+                    if not l.lstrip()[0] == '!':
+                        count = count + 1 
+                    if l.lstrip()[0:5] == "!$acc":
+                        count = count + 1 
             files.append((filepath, count))
             total += count
 
