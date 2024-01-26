a88e559bb731ed326fc59faa4237d93b95318a23
Spencer Bryngelson <sbryngelson@gmail.com>
2024-01-26 16:34:05 -0500

    fix yml

--- misc/run-rg-gpu.sh
+++ misc/run-rg-gpu.sh
@@ -26,4 +26,4 @@ module load nvhpc cmake
 gpu_count=$(nvidia-smi -L | wc -l)        # number of GPUs on node
 gpu_ids=$(seq -s ' ' 0 $(($gpu_count-1))) # 0,1,2,...,gpu_count-1
 
-./mfc.sh test -j 1 --gpu -g $gpu_ids
+./mfc.sh test -j 1 --gpu -g $gpu_ids -- -b mpirun
