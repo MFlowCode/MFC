e521a8a27a249d78ce9527401d66f55c47d0a4e6
Henry Le Berre <hberre3@gatech.edu>
2024-03-18 19:17:39 -0400

    Upstream fixes and tools from Chemfc (#376)

--- toolchain/bootstrap/format.sh
+++ toolchain/bootstrap/format.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -j|--jobs)
+            JOBS="$2"
+            shift
+            ;;
+        *)
+            echo "Format, unknown argument: $1."
+            exit 1
+            ;;
+    esac
+
+    shift
+done
+
 log "Formatting MFC:"
 
-fortran_files=$(find ${@:-src} -type f | grep -Ev 'src/.+/autogen/')
-
-longest=0
-for filepath in $fortran_files; do
-    if [ "${#filepath}" -gt "$longest" ]; then
-        longest="${#filepath}"
-    fi
-done
-
-for filepath in $fortran_files; do
-    echo -n " > $filepath $(printf '%*s' "$((longest - ${#filepath}))" '')"
-
-    before=$(sha256sum "$filepath" | cut -d' ' -f1)
-
-    python3 toolchain/indenter.py "$filepath"
-
-    if ! fprettify "$filepath" \
-        --silent --indent 4 --c-relations --enable-replacements --enable-decl \
-        --whitespace-comma 1 --whitespace-multdiv 0 --whitespace-plusminus 1 \
-        --case 1 1 1 1 --strict-indent --line-length 1000; then
-        error "failed to execute fprettify."
-        error "MFC has not been fprettify'ied."
-        exit 1
-    fi
-
-    after=$(sha256sum "$filepath" | cut -d' ' -f1)
-
-    if [ "$before" != "$after" ]; then
-        echo -e "$YELLOW[formatted]$COLOR_RESET"
-    else
-        echo -e "$GREEN[unchanged]$COLOR_RESET"
-    fi
-done
+if ! find ${@:-src} -type f | grep -Ev 'autogen' | grep -E '\.(f90|fpp)$' | \
+         parallel --jobs ${JOBS:-1} -- \
+             echo      "\> {}" \&\& \
+             python3   toolchain/indenter.py "{}" \&\& \
+             fprettify "{}" --silent --indent 4 --c-relations --enable-replacements \
+                       --enable-decl --whitespace-comma 1 --whitespace-multdiv 0    \
+                       --whitespace-plusminus 1 --case 1 1 1 1 --strict-indent      \
+                       --line-length 1000\;; then
+    error "Formatting MFC failed."
+    exit 1
+fi
 
 ok "Done. MFC has been formatted."
+
