f23fceb103412864a9846fd75fdc13261699e147
Ben Wilfong <48168887+wilfonba@users.noreply.github.com>
2024-05-14 16:08:41 -0400

    Fix Frontier performance regression (#413)
    Co-authored-by: Henry LE BERRE <hberre3@gatech.edu>
    Co-authored-by: wilfonba <bwilfong3@gatech.edu>

--- toolchain/mfc/run/case_dicts.py
+++ toolchain/mfc/run/case_dicts.py
@@ -186,7 +186,7 @@ for fl_id in range(1,10+1):
 
 ALL = list(set(PRE_PROCESS + SIMULATION + POST_PROCESS))
 
-CASE_OPTIMIZATION = [ "nb", "weno_order" ]
+CASE_OPTIMIZATION = [ "nb", "weno_order", "num_fluids"]
 
 
 def get_input_dict_keys(target_name: str) -> list:
