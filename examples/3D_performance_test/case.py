330c6f40d3ee4bdd3b2cad9f85dc03f2f73052ab
Spencer Bryngelson <sbryngelson@gmail.com>
2024-05-25 08:29:06 -0400

    remove new benchmark, cleanup docs, add performance case to examples (i think it is right?)

---
+++ examples/3D_performance_test/case.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import json
+
+# Configuring case dictionary
+print(json.dumps({
+    # Logistics ================================================================
+    'run_time_info'                : 'T',
+    # ==========================================================================
+
+    # Computational Domain Parameters ==========================================
+    'x_domain%beg'                 : 0.E+00,
+    'x_domain%end'                 : 4.E-03/1.E-03,
+    'y_domain%beg'                 : 0.E+00,
+    'y_domain%end'                 : 4.E-03/1.E-03,
+    'z_domain%beg'                 : 0.E+00,
+    'z_domain%end'                 : 4.E-03/1.E-03,
+    'stretch_x'                    : 'T',
+    'a_x'                          : 4.E+00,
+    'x_a'                          : -1.5E-03/1.E-03,
+    'x_b'                          : 1.5E-03/1.E-03,
+    'stretch_y'                    : 'T',
+    'a_y'                          : 4.E+00,
+    'y_a'                          : -1.5E-03/1.E-03,
+    'y_b'                          : 1.5E-03/1.E-03,
+    'stretch_z'                    : 'T',
+    'a_z'                          : 4.E+00,
+    'z_a'                          : -1.5E-03/1.E-03,
+    'z_b'                          : 1.5E-03/1.E-03,
+    'cyl_coord'                    : 'F',
+    'm'                            : 200,
+    'n'                            : 200,
+    'p'                            : 200,
+    'dt'                           : 0.2E-09/1.E-03,
+    't_step_start'                 : 0,
+    't_step_stop'                  : 30,
+    't_step_save'                  : 30,
+    # ==========================================================================
+    
+    # Simulation Algorithm Parameters ==========================================
+    'num_patches'                  : 2,
+    'model_eqns'                   : 2,
+    'alt_soundspeed'               : 'F',
+    'num_fluids'                   : 2,
+    'adv_alphan'                   : 'T',
+    'mpp_lim'                      : 'T',
+    'mixture_err'                  : 'T',
+    'time_stepper'                 : 3,
+    'weno_order'                   : 5,
+    'weno_eps'                     : 1.E-16,
+    'weno_Re_flux'                 : 'F',  
+    'weno_avg'                     : 'F',
+    'avg_state'                    : 2,
+    'mapped_weno'                  : 'T',
+    'null_weights'                 : 'F',
+    'mp_weno'                      : 'F',
+    'riemann_solver'               : 2,
+    'wave_speeds'                  : 1,
+    'bc_x%beg'                     : -2,
+    'bc_x%end'                     : -6,
+    'bc_y%beg'                     : -2,
+    'bc_y%end'                     : -6,
+    'bc_z%beg'                     : -2,
+    'bc_z%end'                     : -6,
+    # ==========================================================================
+
+    # Formatted Database Files Structure Parameters ============================
+    'format'                       : 1,
+    'precision'                    : 2,
+    'prim_vars_wrt'                :'T',
+    'parallel_io'                  :'T',
+    # ==========================================================================
+
+    # Patch 1: High pressured water ============================================
+    'patch_icpp(1)%geometry'       : 9,
+    'patch_icpp(1)%x_centroid'     : 80.E-03/1.E-03,
+    'patch_icpp(1)%y_centroid'     : 80.E-03/1.E-03,
+    'patch_icpp(1)%z_centroid'     : 80.E-03/1.E-03,
+    'patch_icpp(1)%length_x'       : 160.E-03/1.E-03,
+    'patch_icpp(1)%length_y'       : 160.E-03/1.E-03,
+    'patch_icpp(1)%length_z'       : 160.E-03/1.E-03,
+    'patch_icpp(1)%vel(1)'         : 0.E+00,
+    'patch_icpp(1)%vel(2)'         : 0.E+00,
+    'patch_icpp(1)%vel(3)'         : 0.E+00,
+    'patch_icpp(1)%pres'           : 1.E+05,
+    'patch_icpp(1)%alpha_rho(1)'   : 1000.E+00,
+    'patch_icpp(1)%alpha_rho(2)'   : 0.1E+00,
+    'patch_icpp(1)%alpha(1)'       : 0.9E+00,
+    'patch_icpp(1)%alpha(2)'       : 0.1E+00,
+    # ==========================================================================
+
+    # Patch 3: Air bubble ======================================================
+    'patch_icpp(2)%geometry'       : 8,
+    'patch_icpp(2)%smoothen'       : 'T',
+    'patch_icpp(2)%smooth_patch_id' : 1,
+    'patch_icpp(2)%smooth_coeff'   : 0.5E+00,
+    'patch_icpp(2)%x_centroid'     : 0.E+00,
+    'patch_icpp(2)%y_centroid'     : 0.E+00,
+    'patch_icpp(2)%z_centroid'     : 0.E+00,
+    'patch_icpp(2)%radius'         : 1.E-03/1.E-03,
+    'patch_icpp(2)%alter_patch(1)' : 'T',
+    'patch_icpp(2)%vel(1)'         : 0.E+00,
+    'patch_icpp(2)%vel(2)'         : 0.E+00,
+    'patch_icpp(2)%vel(3)'         : 0.E+00,
+    'patch_icpp(2)%pres'           : 1.E+03,
+    'patch_icpp(2)%alpha_rho(1)'   : 100.E+00,
+    'patch_icpp(2)%alpha_rho(2)'   : 0.9E+00,
+    'patch_icpp(2)%alpha(1)'       : 0.1E+00,
+    'patch_icpp(2)%alpha(2)'       : 0.9E+00,
+    # ==========================================================================
+
+    # Fluids Physical Parameters ===============================================
+    'fluid_pp(1)%gamma'            : 1.E+00/(4.4E+00-1.E+00),
+    'fluid_pp(1)%pi_inf'           : 4.4E+00*6.E+08/(4.4E+00-1.E+00),
+    'fluid_pp(2)%gamma'            : 1.E+00/(1.4E+00-1.E+00),
+    'fluid_pp(2)%pi_inf'           : 0.E+00,
+    # ==========================================================================
+}))
+
+# ==============================================================================
