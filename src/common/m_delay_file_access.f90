a4930d6b5190b9f6e7b075667d7d3fd1b1a66415
Brian Ok <63064763+okBrian@users.noreply.github.com>
2024-06-20 16:56:31 -0700

    Common Directory, Formatting and Added Intent (#476)

--- src/common/m_delay_file_access.f90
+++ src/common/m_delay_file_access.f90
@@ -2,8 +2,7 @@ module m_delay_file_access
     implicit none
     private
 
-    public :: &
-        DelayFileAccess
+    public :: DelayFileAccess
 
     integer, private, parameter :: &
         N_PROCESSES_FILE_ACCESS = 128, &
@@ -12,16 +11,10 @@ module m_delay_file_access
 contains
 
     subroutine DelayFileAccess(ProcessRank)
+        integer, intent(in) :: ProcessRank
 
-        integer, intent(in) :: &
-            ProcessRank
-
-        integer :: &
-            iDelay, &
-            nFileAccessDelayIterations
-        real(kind(0d0)) :: &
-            Number, &
-            Dummy
+        integer :: iDelay, nFileAccessDelayIterations
+        real(kind(0d0)) :: Number, Dummy
 
         nFileAccessDelayIterations &
             = (ProcessRank/N_PROCESSES_FILE_ACCESS)*FILE_ACCESS_DELAY_UNIT
