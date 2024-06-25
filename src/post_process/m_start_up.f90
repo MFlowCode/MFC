8956d741f19c75d378b0353b2b89ec19dd31d44b
Brian Ok <63064763+okBrian@users.noreply.github.com>
2024-06-25 09:35:23 -0700

    Post_Process, Formatting (#489)

--- src/post_process/m_start_up.f90
+++ src/post_process/m_start_up.f90
@@ -137,7 +137,7 @@ contains
 
     subroutine s_perform_time_step(t_step)
 
-        integer, intent(INOUT) :: t_step
+        integer, intent(inout) :: t_step
         if (proc_rank == 0) then
             print '(" ["I3"%]  Saving "I8" of "I0" @ t_step = "I0"")', &
                 int(ceiling(100d0*(real(t_step - t_step_start)/(t_step_stop - t_step_start + 1)))), &
@@ -164,9 +164,9 @@ contains
 
     subroutine s_save_data(t_step, varname, pres, c, H)
 
-        integer, intent(INOUT) :: t_step
-        character(LEN=name_len), intent(INOUT) :: varname
-        real(kind(0d0)), intent(INOUT) :: pres, c, H
+        integer, intent(inout) :: t_step
+        character(LEN=name_len), intent(inout) :: varname
+        real(kind(0d0)), intent(inout) :: pres, c, H
 
         integer :: i, j, k, l
 
@@ -620,7 +620,7 @@ contains
         call s_close_formatted_database_file()
     end subroutine s_save_data
 
-    subroutine s_initialize_modules()
+    subroutine s_initialize_modules
         ! Computation of parameters, allocation procedures, and/or any other tasks
         ! needed to properly setup the modules
         call s_initialize_global_parameters_module()
@@ -644,7 +644,7 @@ contains
         end if
     end subroutine s_initialize_modules
 
-    subroutine s_initialize_mpi_domain()
+    subroutine s_initialize_mpi_domain
         ! Initialization of the MPI environment
         call s_mpi_initialize()
 
@@ -669,7 +669,7 @@ contains
 
     end subroutine s_initialize_mpi_domain
 
-    subroutine s_finalize_modules()
+    subroutine s_finalize_modules
         ! Disassociate pointers for serial and parallel I/O
         s_read_data_files => null()
 
