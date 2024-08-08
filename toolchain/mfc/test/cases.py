6549de587f10ec833d35b2f059cfee3fe20f3d97
Hyeoksu Lee <hlee3@dt-login02.delta.ncsa.illinois.edu>
2024-08-08 01:20:23 -0500

    update tests

--- toolchain/mfc/test/cases.py
+++ toolchain/mfc/test/cases.py
@@ -510,14 +510,14 @@ def list_cases() -> typing.List[TestCaseBuilder]:
             for bubbles in ['F', 'T']:
                 stack.push('instability_wave', {
                     'm': 24, 'n': 49, 'num_patches': 1, 'num_fluids': 1,
-                    'x_domain%beg': 0.0, 'x_domain%end': 40.0, 'y_domain%beg': -20.0, 'y_domain%end': 20.0,
-                    'bc_x%beg': -1, 'bc_x%end': -1, 'bc_y%beg': -6, 'bc_y%end': -6,
-                    'vel_profile': 'T', 'vel_profile_domain': 1.475, 'vel_profile_coef': 0.04, 'instability_wave': 'T',
+                    'x_domain%beg': 0.0, 'x_domain%end': 360.0, 'y_domain%beg': -180.0, 'y_domain%end': 180.0,
+                    'bc_x%beg': -3, 'bc_x%end': -3, 'bc_y%beg': -6, 'bc_y%end': -6,
+                    'vel_profile': 'T', 'vel_profile_domain': 1.475, 'vel_profile_coef': 0.4, 'instability_wave': 'T',
                     'weno_Re_flux': 'T', 'weno_avg': 'T', 'mapped_weno': 'T',
                     'fluid_pp(1)%gamma': 0.16393442623, 'fluid_pp(1)%pi_inf': 22.312399959394575,  
                     'fluid_pp(1)%Re(1)': 1.6881644098979287,
-                    'patch_icpp(1)%x_centroid': 20.0, 'patch_icpp(1)%length_x': 40.0,
-                    'patch_icpp(1)%y_centroid': 0.0, 'patch_icpp(1)%length_y': 40.0, 
+                    'patch_icpp(1)%x_centroid': 180.0, 'patch_icpp(1)%length_x': 360.0,
+                    'patch_icpp(1)%y_centroid': 0.0, 'patch_icpp(1)%length_y': 360.0, 
                     'patch_icpp(1)%vel(1)': 1.1966855884162177, 'patch_icpp(1)%vel(2)': 0.0, 'patch_icpp(1)%pres': 1.0, 
                     'patch_icpp(2)%geometry': -100, 'patch_icpp(2)%x_centroid': -1e6, 'patch_icpp(2)%length_x': -1e6,
                     'patch_icpp(2)%y_centroid': -1e6, 'patch_icpp(2)%length_y': -1e6, 'patch_icpp(2)%vel(1)': -1e6,
@@ -547,8 +547,8 @@ def list_cases() -> typing.List[TestCaseBuilder]:
 
                 if len(dimInfo[0]) == 3:
                     stack.push('', {'p': 24, 'patch_icpp(1)%geometry': 9,
-                        'z_domain%beg': 0.0, 'z_domain%end': 20.0, 'bc_z%beg': -1, 'bc_z%end': -1,
-                        'patch_icpp(1)%z_centroid': 20.0, 'patch_icpp(1)%length_z': 40.0, 'patch_icpp(1)%vel(3)': 0.0,
+                        'z_domain%beg': 0.0, 'z_domain%end': 360.0, 'bc_z%beg': -3, 'bc_z%end': -3,
+                        'patch_icpp(1)%z_centroid': 180.0, 'patch_icpp(1)%length_z': 360.0, 'patch_icpp(1)%vel(3)': 0.0,
                         'patch_icpp(2)%z_centroid': -1e6, 'patch_icpp(2)%length_z': -1e6, 'patch_icpp(2)%vel(3)': -1e6,
                         'patch_icpp(3)%z_centroid': -1e6, 'patch_icpp(3)%length_z': -1e6, 'patch_icpp(3)%vel(3)': -1e6
                     })
