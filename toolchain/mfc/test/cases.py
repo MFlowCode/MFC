f2d33e06d2e58b796f8f5289fb649c93ef68ae88
Brian Ok <brianok924@gmail.com>
2024-09-13 11:42:31 -0400

    add support ibm_cfl_dt

--- toolchain/mfc/test/cases.py
+++ toolchain/mfc/test/cases.py
@@ -718,8 +718,6 @@ def list_cases() -> typing.List[TestCaseBuilder]:
         for path in os.listdir(common.MFC_EXAMPLE_DIRPATH):
             if path == "scaling":
                 continue
-            if path == "2D_ibm_cfl_dt":
-                continue
 
             name = f"{path.split('_')[0]} -> Example -> {'_'.join(path.split('_')[1:])}"
             path = os.path.join(common.MFC_EXAMPLE_DIRPATH, path, "case.py")
@@ -727,7 +725,7 @@ def list_cases() -> typing.List[TestCaseBuilder]:
                 continue
             def modify_example_case(case: dict):
                 case['parallel_io'] = 'F'
-                if case['t_step_stop'] >= 50:
+                if 't_step_stop' in case['t_step_stop'] >= 50:
                     case['t_step_start'] = 0
                     case['t_step_stop'] = 50
                     case['t_step_save'] = 50
