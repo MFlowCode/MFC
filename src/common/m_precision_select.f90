867a17f75f5b928c84918ad6205b7618b72cc45f
aricer123 <107273558+aricer123@users.noreply.github.com>
2024-09-23 12:20:51 -0400

    Update m_precision_select.f90
    Check if old method of checking MPI support passes mpi tests

--- src/common/m_precision_select.f90
+++ src/common/m_precision_select.f90
@@ -1,41 +1,23 @@
-!> @file m_precision_select.f90
-!> @brief Contains module m_precision_select
+!>
+!! @file m_precision_select.f90
+!! @brief Contains module m_precision_select
 
 !> @brief This file contains the definition of floating point used in MFC
 module m_precision_select
 #ifdef MFC_MPI
-    use mpi                    !< Message Passing Interface (MPI) module
+    use mpi                    !< Message passing interface (MPI) module
 #endif
 
     implicit none
 
-    ! Define the available precision types
     integer, parameter :: single_precision = selected_real_kind(6, 37)
     integer, parameter :: double_precision = selected_real_kind(15, 307)
 
-    ! Set the working precision (wp) to single or double precision
-    integer, parameter :: wp = double_precision  ! Change this to single_precision if needed
-
+    integer, parameter :: wp = double_precision
 #ifdef MFC_MPI
-    ! Declare mpi_p as a module variable
-    integer :: mpi_p
+    integer, parameter :: mpi_p = MPI_DOUBLE_PRECISION
 #else
     integer, parameter :: mpi_p = -100
 #endif
-
-contains
-
-    ! Subroutine to initialize mpi_p based on wp
-    subroutine initialize_precision()
-#ifdef MFC_MPI
-        if (wp == single_precision) then
-            mpi_p = MPI_FLOAT
-        else if (wp == double_precision) then
-            mpi_p = MPI_DOUBLE_PRECISION
-        else
-            stop 'Unsupported precision kind.'
-        end if
-#endif
-    end subroutine initialize_precision
 
 end module m_precision_select
