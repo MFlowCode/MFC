32f6b6988082659a0382486aca91082544839f21
Spencer Bryngelson <shb@gatech.edu>
2024-11-26 21:41:06 -0500

    Update submit.sh

--- .github/workflows/delta/submit.sh
+++ .github/workflows/delta/submit.sh
@@ -21,7 +21,7 @@ sbatch_cpu_opts="\
 sbatch_gpu_opts="\
 #SBATCH -p gpuA100x4,gpuA100x8,gpuA100x4-interactive,gpuA100x8-interactive
 #SBATCH --account=bdiy-delta-gpu
-#SBATCH --gpus-per-node=4
+#SBATCH --gpus-per-node=2
 "
 
 if [ "$2" == "cpu" ]; then
