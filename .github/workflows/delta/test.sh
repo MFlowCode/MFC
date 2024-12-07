3eb1706ae68070fd0fd433b0b94b05c51e19783a
Spencer Bryngelson <shb@gatech.edu>
2024-12-07 09:27:29 -0500

    Update test.sh

--- .github/workflows/delta/test.sh
+++ .github/workflows/delta/test.sh
@@ -7,7 +7,7 @@ fi
 
 ./mfc.sh test --dry-run -j 20 $build_opts
 
-n_test_threads=8
+n_build_threads=20
 
 if [ "$job_device" == "gpu" ]; then
     gpu_count=$(nvidia-smi -L | wc -l)        # number of GPUs on node
@@ -17,5 +17,5 @@ if [ "$job_device" == "gpu" ]; then
 fi
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/sw/spack/deltas11-2023-03/apps/linux-rhel8-zen3/nvhpc-24.1/openmpi-4.1.5-zkiklxi/lib/
-./mfc.sh test --max-attempts 3 -a -j $n_test_threads $device_opts -- -c delta
-
+./mfc.sh build -j $n_build_threads --gpu
+./mfc.sh test --max-attempts 3 -a -j $n_test_threads $device_opts --no-build -- -c delta
