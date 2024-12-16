bf47549c07e3bda1b48853aa65c5ec3989014758
Spencer Bryngelson <shb@gatech.edu>
2024-12-16 09:18:33 -0500

    Discard changes to .github/workflows/frontier/test.sh

--- .github/workflows/frontier/test.sh
+++ .github/workflows/frontier/test.sh
@@ -4,3 +4,4 @@ gpus=`rocm-smi --showid | awk '{print $1}' | grep -Eo '[0-9]+' | uniq | tr '\n' 
 ngpus=`echo "$gpus" | tr -d '[:space:]' | wc -c`
 
 ./mfc.sh test --max-attempts 3 -j $ngpus -- -c frontier
+
