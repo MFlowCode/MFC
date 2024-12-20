6dbab6870ab0fc6848fe2bd6ce27d02fe927ce53
Spencer Bryngelson <shb@gatech.edu>
2024-12-20 13:11:14 -0500

    Update submit.sh

--- .github/workflows/frontier/submit.sh
+++ .github/workflows/frontier/submit.sh
@@ -23,6 +23,7 @@ sbatch <<EOT
 #SBATCH -n 8                       # Number of cores required
 #SBATCH -t 02:00:00                # Duration of the job (Ex: 15 mins)
 #SBATCH -o$job_slug.out            # Combined output and error messages file
+#SBATCH -q debug                   # Use debug QOS - only one job per user allowed in queue!
 #SBATCH -W                         # Do not exit until the submitted job terminates.
 
 set -e
