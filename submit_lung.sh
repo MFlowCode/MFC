25bf4fe60491bcae210772e635506673e4762c4c
Mauro Rodriguez <mauro_rodriguez@brown.edu>
2024-12-21 15:43:20 -0600

    cleaning up the master branch in preparation for PR

--- submit_lung.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#./mfc.sh run ./examples/3D_lungwave/case.py -p gpuA100x4 -N 1 -n 2 -g 2 -w 01:00:00 -# test1 -t pre_process -a bciv-delta-gpu -c delta
-#./mfc.sh run ./examples/3D_lungwave/case.py -p gpuA100x4 -N 1 -n 2 -g 2 -w 01:00:00 -# test1 -t simulation -a bciv-delta-gpu -c delta
-#./mfc.sh run ./examples/3D_lungwave/case.py -p gpuA100x4 -N 1 -n 1 -g 1 -w 01:00:00 -# test1 -t post_process -a bciv-delta-gpu -c delta
-
-./mfc.sh run /scratch/bciv/rodrigu1/lung/case.py -p gpuA100x4 -N 1 -n 2 -g 1 -w 01:00:00 -# test1 -t pre_process -a bciv-delta-gpu -c delta
-./mfc.sh run /scratch/bciv/rodrigu1/lung/case.py -p gpuA100x4 -N 1 -n 2 -g 1 -w 01:00:00 -# test1 -t simulation -a bciv-delta-gpu -c delta
-
