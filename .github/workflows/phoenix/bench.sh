658f82e19681666a47e518f9364dfe53699fa081
malmahrouqi3 <malmahrouqi3@gatech.edu>
2025-07-08 12:20:09 -0400

    added nsight profiling to phoenix benchmark cases

--- .github/workflows/phoenix/bench.sh
+++ .github/workflows/phoenix/bench.sh
@@ -16,9 +16,9 @@ mkdir -p $currentdir
 export TMPDIR=$currentdir
 
 if [ "$job_device" = "gpu" ]; then
-    ./mfc.sh bench --mem 12 -j $(nproc) -o "$job_slug.yaml" -- -c phoenix-bench $device_opts -n $n_ranks
+    nsys profile -o report ./mfc.sh bench --mem 12 -j $(nproc) -o "$job_slug.yaml" -- -c phoenix-bench $device_opts -n $n_ranks
 else
-    ./mfc.sh bench --mem 1 -j $(nproc) -o "$job_slug.yaml" -- -c phoenix-bench $device_opts -n $n_ranks
+    nsys profile -o report ./mfc.sh bench --mem 1 -j $(nproc) -o "$job_slug.yaml" -- -c phoenix-bench $device_opts -n $n_ranks
 fi
 
 sleep 10
