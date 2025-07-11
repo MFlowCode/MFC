77feee97d14ddeba6004452c3b5fd8fb533c1d7d
Spencer Bryngelson <shb@gatech.edu>
2025-07-11 13:32:11 -0400

    Update bench.sh

--- .github/workflows/phoenix/bench.sh
+++ .github/workflows/phoenix/bench.sh
@@ -2,7 +2,8 @@
 
 n_ranks=12
 
-if [ "$job_device" = "gpu" ]; then
+echo "My benchmarking device is:" $device
+if [ "$device" = "gpu" ]; then
     n_ranks=$(nvidia-smi -L | wc -l)        # number of GPUs on node
     gpu_ids=$(seq -s ' ' 0 $(($n_ranks-1))) # 0,1,2,...,gpu_count-1
     device_opts="--gpu -g $gpu_ids"
