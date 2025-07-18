05df25ca0f9d8e41933a78abd65116106c72663f
Spencer Bryngelson <sbryngelson@gmail.com>
2025-07-18 11:57:52 -0400

    fix

--- .github/workflows/frontier/test.sh
+++ .github/workflows/frontier/test.sh
@@ -4,7 +4,7 @@ gpus=`rocm-smi --showid | awk '{print $1}' | grep -Eo '[0-9]+' | uniq | tr '\n' 
 ngpus=`echo "$gpus" | tr -d '[:space:]' | wc -c`
 
 if [ "$job_device" = "gpu" ]; then
-    ./mfc.sh test --max-attempts 3 -j $ngpus -- -c frontier
+    ./mfc.sh test --max-attempts 3 -j $ngpus --sched-debug -- -c frontier
 else
-    ./mfc.sh test --max-attempts 3 -j 32 -- -c frontier
+    ./mfc.sh test --max-attempts 3 -j 32 --sched-debug -- -c frontier
 fi
