e966781b36e85fcda0e2c5ffb5332d51ffe73db1
Mohammed S. Al-Mahrouqi <145478595+Malmahrouqi3@users.noreply.github.com>
2025-09-15 15:01:12 -0400

    `--rdma-mpi` flag fix (#996)
    Co-authored-by: Malmahrouqi3 <mohdsaid497566@gmail.com>

--- .github/workflows/frontier/build.sh
+++ .github/workflows/frontier/build.sh
@@ -13,6 +13,6 @@ if [ "$2" == "bench" ]; then
         ./mfc.sh run "$dir/case.py" --case-optimization -j 8 --dry-run $build_opts
     done
 else
-    ./mfc.sh test -a --dry-run --rdma-mpi --generate -j 8 $build_opts
+    ./mfc.sh test -a --dry-run --rdma-mpi -j 8 $build_opts
 fi
 
