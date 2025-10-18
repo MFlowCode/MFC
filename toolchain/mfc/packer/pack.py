f1ef60be4d9f35856d393e97dcb030ea7ad3d079
Ben Wilfong <48168887+wilfonba@users.noreply.github.com>
2025-10-18 11:24:52 -0400

    I/O Improvements for run time info and EL Bubbles (#1015)
    Co-authored-by: Spencer Bryngelson <sbryngelson@gmail.com>

--- toolchain/mfc/packer/pack.py
+++ toolchain/mfc/packer/pack.py
@@ -126,17 +126,23 @@ def compile(casepath: str) -> typing.Tuple[Pack, str]:
         short_filepath = str(filepath).replace(f'{case_dir}', '')[1:].replace("\\", "/")
         content        = common.file_read(filepath)
 
-        try:
-            # Takes a string of numbers and returns them as a list of floats.
-            def _extract_doubles(s: str) -> list:
-                return [ float(e) for e in re.sub(r"[\n\t\s]+", " ", s).strip().split(' ') ]
+        # Takes a string of numbers and returns them as a list of floats.
+        def _extract_doubles(s: str) -> list:
+            return [ float(e) for e in re.sub(r"[\n\t\s]+", " ", s).strip().split(' ') ]
 
-            # Every line is <x> <y> <z> <value> (<y> and <z> are optional). So the
-            # number of dimensions is the number of doubles in the first line minus 1.
-            ndims   = len(_extract_doubles(content.split('\n', 1)[0])) - 1
-            # We discard all <x> <y> <z> values and only keep the <value> ones.
-            # This is in an effort to save on storage.
-            doubles = _extract_doubles(content)[ndims::ndims+1]
+
+        try:
+            if "lag_bubble" in short_filepath:
+                lines = content.splitlines()
+                content = "\n".join(lines[1:])  # Skip the first line
+                doubles = _extract_doubles(content)
+            else:
+                # Every line is <x> <y> <z> <value> (<y> and <z> are optional). So the
+                # number of dimensions is the number of doubles in the first line minus 1.
+                ndims   = len(_extract_doubles(content.split('\n', 1)[0])) - 1
+                # We discard all <x> <y> <z> values and only keep the <value> ones.
+                # This is in an effort to save on storage.
+                doubles = _extract_doubles(content)[ndims::ndims+1]
         except ValueError:
             return None, f"Failed to interpret the content of [magenta]{filepath}[/magenta] as a list of floating point numbers."
 
