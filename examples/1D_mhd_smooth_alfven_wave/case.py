b4db3c3b74188b0404b21876239df55c30670090
Daniel J Vickers <dnlvickers5@gmail.com>
2025-11-12 11:45:13 -0500

    Added smooth case for alfvin waves in 1D

--- examples/1D_mhd_smooth_alfven_wave/case.py
+++ examples/1D_mhd_smooth_alfven_wave/case.py
@@ -2,31 +2,25 @@
 import json
 import math
 
-# Case for smooth Alfven waves from:
-# Implicit hybridized discontinuous Galerkin methods for
-# compressible magnetohydrodynamics
-# C. Ciuca, P. Fernandez, A. Christophe, N.C. Nguyen, J. Peraire
+# Case for smooth Alfven waves from section 6.3.1 of:
+# The ∇·B=0 Constraint in Shock-Capturing Magnetohydrodynamics Codes
+# Gábor Tóth
 
-# A 2D magnetic vortex advects at for a period of T=10 diagonally
-
-# Configuring case dictionary
 print(
     json.dumps(
         {
             # Logistics
             "run_time_info": "T",
             # Computational Domain Parameters
-            "x_domain%beg": -5.0,
-            "x_domain%end": 5.0,
-            "y_domain%beg": -5.0,
-            "y_domain%end": 5.0,
-            "m": 512,
-            "n": 512,
+            "x_domain%beg": 0.0,
+            "x_domain%end": 1.0,
+            "m": 1023,
+            "n": 0,
             "p": 0,
-            "dt": 1.0e-4,
+            "dt": 1.0e-5,
             "t_step_start": 0,
             "t_step_stop": 100000,
-            "t_step_save": 100000,
+            "t_step_save": 10000,
             # Simulation Algorithm Parameters
             "num_patches": 1,
             "model_eqns": 2,
@@ -35,8 +29,8 @@ print(
             "mpp_lim": "F",
             "mixture_err": "F",
             "time_stepper": 3,
-            "weno_order": 5,
-            "weno_eps": 1.0e-12,
+            "weno_order": 3,
+            "weno_eps": 1e-7,
             "null_weights": "F",
             "mp_weno": "F",
             "riemann_solver": 4,
@@ -44,8 +38,6 @@ print(
             "avg_state": 2,
             "bc_x%beg": -1,
             "bc_x%end": -1,
-            "bc_y%beg": -1,
-            "bc_y%end": -1,
             # Formatted Database Files Structure Parameters
             "format": 1,
             "precision": 2,
@@ -54,20 +46,18 @@ print(
             "parallel_io": "F",
             # MHD
             "mhd": "T",
-            "patch_icpp(1)%hcid": 252,
-            "patch_icpp(1)%geometry": 3,
-            "patch_icpp(1)%x_centroid": 0.0,
-            "patch_icpp(1)%y_centroid": 0.0,
-            "patch_icpp(1)%length_x": 10.0,
-            "patch_icpp(1)%length_y": 10.0,
+            "Bx0": 1.0,
+            "patch_icpp(1)%geometry": 1,
+            "patch_icpp(1)%hcid": 150,
+            "patch_icpp(1)%x_centroid": 0.5,
+            "patch_icpp(1)%length_x": 1.0,
             "patch_icpp(1)%vel(1)": 0.0,
             "patch_icpp(1)%vel(2)": 0.0,
             "patch_icpp(1)%vel(3)": 0.0,
-            "patch_icpp(1)%pres": 5.0 / (12 * math.pi),
-            "patch_icpp(1)%Bx": 0.0,
+            "patch_icpp(1)%pres": 0.1,
             "patch_icpp(1)%By": 0.0,
             "patch_icpp(1)%Bz": 0.0,
-            "patch_icpp(1)%alpha_rho(1)": 25.0 / (36.0 * math.pi),
+            "patch_icpp(1)%alpha_rho(1)": 1.0,
             "patch_icpp(1)%alpha(1)": 1.0,
             # Fluids Physical Parameters
             "fluid_pp(1)%gamma": 1.0e00 / (5.0 / 3.0 - 1.0e00),
