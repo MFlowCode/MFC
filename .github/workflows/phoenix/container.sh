2fe50848a26cd16f3a33bd26bbe24b57be38870c
Mohammed S. Al-Mahrouqi <145478595+Malmahrouqi3@users.noreply.github.com>
2025-12-15 00:25:54 -0500

    Fix quotes in GPU ID sequence generation

--- .github/workflows/phoenix/container.sh
+++ .github/workflows/phoenix/container.sh
@@ -34,7 +34,7 @@ n_test_threads=8
 
 if [ "$job_device" = "gpu" ]; then
     gpu_count=$(nvidia-smi -L | wc -l)        # number of GPUs on node
-    gpu_ids=$(seq -s ' ' 0 $(($gpu_count-1))) # 0,1,2,...,gpu_count-1
+    gpu_ids=$(seq -s " " 0 $(($gpu_count-1))) # 0,1,2,...,gpu_count-1
     device_opts="-g $gpu_ids"
     n_test_threads=`expr $gpu_count \* 2`
 fi
