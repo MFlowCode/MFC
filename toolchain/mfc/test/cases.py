96b8e46414d7125d2359d0a007a4b35045abf97e
danieljvickers <dnlvickers5@gmail.com>
2026-02-22 01:49:24 -0500

    Never saved change to skipped case

--- toolchain/mfc/test/cases.py
+++ toolchain/mfc/test/cases.py
@@ -1040,7 +1040,8 @@ def list_cases() -> typing.List[TestCaseBuilder]:
                            "2D_backward_facing_step",
                            "2D_forward_facing_step",
                            "1D_convergence",
-                           "3D_IGR_33jet","1D_multispecies_diffusion"]
+                           "3D_IGR_33jet","1D_multispecies_diffusion",
+                           "2D_ibm_stl_MFCCharacter"]
             if path in casesToSkip:
                 continue
             name = f"{path.split('_')[0]} -> Example -> {'_'.join(path.split('_')[1:])}"
