52e8af133e10d62313fc624a793aac9e99b875ca
Spencer Bryngelson <sbryngelson@gmail.com>
2026-03-25 10:57:10 -0400

    Replace fprettify with ffmt formatter; comment cleanup (#1334)

--- src/common/m_delay_file_access.f90
+++ src/common/m_delay_file_access.f90
@@ -4,30 +4,29 @@
 
 !> @brief Rank-staggered file access delays to prevent I/O contention on parallel file systems
 module m_delay_file_access
+
     use m_precision_select
+
     implicit none
+
     private
 
     public :: DelayFileAccess
 
-    integer, private, parameter :: &
-        N_PROCESSES_FILE_ACCESS = 128, &
-        FILE_ACCESS_DELAY_UNIT = 10000
+    integer, private, parameter :: N_PROCESSES_FILE_ACCESS = 128, FILE_ACCESS_DELAY_UNIT = 10000
 
 contains
 
-    !> @brief Introduces a rank-dependent busy-wait delay to stagger parallel file access and reduce I/O contention.
+    !> Introduce a rank-dependent busy-wait delay to stagger parallel file access and reduce I/O contention.
     impure subroutine DelayFileAccess(ProcessRank)
+
         integer, intent(in) :: ProcessRank
+        integer             :: iDelay, nFileAccessDelayIterations
+        real(wp)            :: Number, Dummy
 
-        integer :: iDelay, nFileAccessDelayIterations
-        real(wp) :: Number, Dummy
-
-        nFileAccessDelayIterations &
-            = (ProcessRank/N_PROCESSES_FILE_ACCESS)*FILE_ACCESS_DELAY_UNIT
+        nFileAccessDelayIterations = (ProcessRank/N_PROCESSES_FILE_ACCESS)*FILE_ACCESS_DELAY_UNIT
 
         do iDelay = 1, nFileAccessDelayIterations
-            ! Wait my turn
             call random_number(Number)
             Dummy = Number*Number
         end do
