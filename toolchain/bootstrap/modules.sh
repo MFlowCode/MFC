0f833872dd1f8ea7968a51ed260dce75ffef69ae
Diego Vaca Revelo <143528660+dgvacarevelo@users.noreply.github.com>
2026-04-03 16:27:59 -0400

    Turing WPI support (#1335)
    Co-authored-by: Diego Vaca <dgvacarevelo@login-01.cm.cluster>

--- toolchain/bootstrap/modules.sh
+++ toolchain/bootstrap/modules.sh
@@ -46,7 +46,8 @@ if [ -v $u_c ]; then
     log   "$BR""Brown$W:   Oscar (o)"
     log   "$B""DoD$W:     Carpenter Cray (cc) | Carpenter GNU (c) |  Nautilus (n)"
     log   "$OR""Florida$W: HiPerGator (h)"
-    log_n "($G""a$W/$G""f$W/$G""s$W/$G""w$W/$B""tuo$W/$C""b$W/$C""e$CR/$C""d/$C""dai$CR/$Y""p$CR/$R""r$CR/$B""cc$CR/$B""c$CR/$B""n$CR/$BR""o"$CR"/$OR""h"$CR"): "
+    log   "$C""WPI $W:   Turing   (t)"
+    log_n "($G""a$W/$G""f$W/$G""s$W/$G""w$W/$B""tuo$W/$C""b$W/$C""e$CR/$C""d/$C""dai$CR/$Y""p$CR/$R""r$CR/$B""cc$CR/$B""c$CR/$B""n$CR/$BR""o"$CR"/$OR""h"$CR/$C""t""$CR"): "
     read u_c
     log
 fi
