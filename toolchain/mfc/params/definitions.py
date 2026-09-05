70589fd57b39a04730100f91e6c07005d000203b
Daniel J. Vickers <dnlvickers5@gmail.com>
2026-09-05 19:42:39 -0400

    Debug wall collisions and particle state writing (#1745)
    Co-authored-by: Spencer Bryngelson <sbryngelson@gmail.com>

--- toolchain/mfc/params/definitions.py
+++ toolchain/mfc/params/definitions.py
@@ -1314,6 +1314,8 @@ _nv(
     "avg_state",
     "alt_soundspeed",
     "mixture_err",
+    "num_particle_clouds",
+    "particle_cloud",
 )
 _nv(
     _PRE_SIM,
@@ -1395,10 +1397,8 @@ _nv(
     "coefficient_of_restitution",
     "collision_time",
     "ib_coefficient_of_friction",
-    "num_particle_clouds",
     "ib_neighborhood_radius",
     "many_ib_patch_parallelism",
-    "particle_cloud",
     "tau_star",
     "cont_damage_s",
     "alpha_bar",
